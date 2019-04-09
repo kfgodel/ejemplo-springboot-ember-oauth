108840424afcf4d5a2444f1f9672f8c2ae4deecf
Dario L Garcia <dario.garcia@10pines.com>
2019-04-09 00:18:01 -0300

    Permitir acceso a '/'

--- springboot-backend/src/main/java/com/example/springbootbackend/config/WebSecurityConfig.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springbootbackend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,6 +19,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .password("password")
         .roles("USER")
         .build());
+  }
+
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+            .antMatchers("/","/assets/**").permitAll()
+            .antMatchers("/api/**").hasRole("USER")
+            .anyRequest().authenticated()
+            .and().formLogin().disable();
+
+    //    http
+//            .authorizeRequests()
+//            .antMatchers(HttpMethod.GET, "/","/static/**", "/resources/**","/resources/public/**").permitAll()
+//            .anyRequest().authenticated()
+//            .and()
+//            .formLogin()
+//            .and()
+//            .httpBasic().disable()
+//            .requiresChannel().anyRequest().requiresSecure();
   }
 
 }
